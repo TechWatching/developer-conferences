baecdb6e060e03b6e7f8eb7c75488b9a6d83c0a7
Alexandre Nédélec <15186176+TechWatching@users.noreply.github.com>
2025-03-29 15:14:16 +0100

    feat: extract a ConferenceCalendar component

---
+++ app/components/ConferenceCalendar.vue
@@ -0,0 +1,137 @@
+<script setup lang="ts">
+import { getLocalTimeZone, type DateValue } from '@internationalized/date'
+import { today } from '@internationalized/date'
+import { type CalendarEvent } from '~~/types/events'
+import type { TableColumn } from '@nuxt/ui'
+
+const props = defineProps<{
+  conferences: CalendarEvent[]
+}>()
+
+const selectedDate = shallowRef(today(getLocalTimeZone()))
+const selectedCountries = ref<string[]>(["France"])
+
+const countries = computed(() => {
+  const uniqueCountries = new Set(
+    props.conferences
+      .map(conf => conf.country)
+      .filter(Boolean)
+  )
+  return Array.from(uniqueCountries).sort()
+})
+
+function isConferenceDate(date: DateValue): boolean {
+  return props.conferences.some((conference: CalendarEvent) => 
+    date.compare(conference.startDate) >= 0 
+    && date.compare(conference.endDate) <= 0 
+    && (selectedCountries.value.length === 0 || selectedCountries.value.includes(conference.country))
+  )
+}
+
+const columnVisiblity = {
+  name: true,
+  location: true,
+  country: false,
+  startDate: false,
+  endDate: false,
+  hyperlink: false,
+  dates: true,
+}
+
+const columns: TableColumn<CalendarEvent>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Conference',
+    cell: ({row}) => h('a', {
+      href: row.getValue('hyperlink'),
+      target: '_blank',
+      class: 'text-primary hover:underline'
+    }, row.getValue('name')),
+  },
+  {
+    accessorKey: 'location',
+    header: 'Location',
+  },
+  {
+    accessorKey: 'country',
+    header: 'Country',
+  },
+  {
+    accessorKey: 'startDate',
+  },
+  {
+    accessorKey: 'endDate',
+  },
+  {
+    "accessorKey": "hyperlink",
+  },
+  {
+    accessorKey: 'dates',
+    header: 'Dates',
+    cell: ({row}) => {
+      const startDate = row.getValue('startDate') as DateValue
+      const endDate = row.getValue('endDate') as DateValue
+      const formatter = new Intl.DateTimeFormat("en-US", {
+        timeZone: getLocalTimeZone(),
+        day: 'numeric',
+        month: 'long',
+      })
+      return startDate.compare(endDate) === 0
+        ? formatter.format(startDate.toDate(getLocalTimeZone()))
+        : `From ${formatter.format(startDate.toDate(getLocalTimeZone()))} to ${formatter.format(endDate.toDate(getLocalTimeZone()))}`
+    }
+  },
+]
+
+const conferencesForSelectedDate = computed(() => {
+  if (!selectedDate.value) return []
+  
+  return props.conferences.filter((conference: CalendarEvent) => 
+    // Filter by date
+    (selectedDate.value.compare(conference.startDate) >= 0 && 
+    selectedDate.value.compare(conference.endDate) <= 0) && 
+    // Filter by countries if any are selected
+    (selectedCountries.value.length === 0 || 
+    selectedCountries.value.includes(conference.country))
+  )
+})
+</script>
+
+<template>
+  <div>
+    <div class="flex justify-center">
+      <UInputMenu
+        v-model="selectedCountries"
+        :items="countries"
+        multiple
+        placeholder="Filter by country"
+        class="w-full max-w-md"
+      />
+    </div>
+
+    <div class="flex flex-col md:flex-row justify-center md:space-x-20 space-y-10 md:space-y-0 mt-10 overflow-y-auto">
+      <div class="calendar-wrapper">
+        <UCalendar 
+          v-model="selectedDate" 
+          size="xl"
+          :ui="{ 
+            cellTrigger: 'size-12 text-lg',
+            headCell: 'text-lg py-2',
+            heading: 'text-xl font-bold py-2'
+          }"
+        >
+          <template #day="{ day }">
+            <UChip :show="isConferenceDate(day)" size="xs">
+              {{ day.day }}
+            </UChip>
+          </template>
+        </UCalendar>
+      </div>
+      <UTable 
+        :data="conferencesForSelectedDate" 
+        :columns="columns" 
+        :column-visibility="columnVisiblity"
+      />
+    </div>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,133 +1,22 @@
 <script setup lang="ts">
-import { getLocalTimeZone, type DateValue } from '@internationalized/date'
-import { today } from '@internationalized/date'
 import { convertEventToCalendarEvent, type CalendarEvent, type DeveloperAgendaConference } from '~~/types/events';
-import type { TableColumn } from '@nuxt/ui'
 
 const { data: allEvents } = await useFetch<DeveloperAgendaConference[]>('https://developers.events/all-events.json')
 const conferences = computed<CalendarEvent[]>(() => {
   if (!allEvents.value) return []
   return allEvents.value.map(event => convertEventToCalendarEvent(event))
 })
-
-function isConferenceDate(date: DateValue): boolean {
-  return conferences.value.some((conference: CalendarEvent) => date.compare(conference.startDate) >= 0 
-    && date.compare(conference.endDate) <= 0 && 
-    (selectedCountries.value.length === 0 || selectedCountries.value.includes(conference.country)));
-}
-
-const columnVisiblity = {
-  name: true,
-  startDate: false,
-  endDate: false,
-  location: true,
-  country: false,
-};
-const columns: TableColumn<CalendarEvent>[] = [
-  {
-    accessorKey: 'name',
-    header: 'Conference',
-    cell: ({row}) => h('a', {
-      href: row.getValue('hyperlink'),
-      target: '_blank',
-      class: 'text-primary hover:underline'
-    }, row.getValue('name')),
-  },
-  {
-    accessorKey: 'location',
-    header: 'Location',
-  },
-  {
-    accessorKey: 'country',
-    header: 'Country',
-  },
-  {
-    accessorKey: 'startDate',
-  },
-  {
-    accessorKey: 'endDate',
-  },
-  {
-    accessorKey: 'dates',
-    header: 'Dates',
-    cell: ({row}) => {
-      const startDate = row.getValue('startDate') as DateValue;
-      const endDate = row.getValue('endDate') as DateValue;
-      const formatter = new Intl.DateTimeFormat("en-US", {
-        timeZone: getLocalTimeZone(),
-        day: 'numeric',
-        month: 'long',
-      });
-      return startDate.compare(endDate) === 0
-        ? formatter.format(startDate.toDate(getLocalTimeZone()))
-        : `From ${formatter.format(startDate.toDate(getLocalTimeZone()))} to ${formatter.format(endDate.toDate(getLocalTimeZone()))}`;
-    }
-  },
-]
-
-const selectedCountries = ref<string[]>(['France']);
-
-const countries = computed(() => {
-  const uniqueCountries = new Set(
-    conferences.value
-      .map(conf => conf.country)
-      .filter(Boolean)
-  );
-  return Array.from(uniqueCountries).sort();
-});
-
-const selectedDate = shallowRef(today(getLocalTimeZone()));
-const conferencesForSelectedDate = computed(() => {
-  if (!selectedDate.value) return [];
-  
-  return conferences.value.filter((conference: CalendarEvent) => 
-    // Filter by date
-    (selectedDate.value.compare(conference.startDate) >= 0 && 
-    selectedDate.value.compare(conference.endDate) <= 0) && 
-    // Filter by countries if any are selected
-    (selectedCountries.value.length === 0 || 
-    selectedCountries.value.includes(conference.country))
-  );
-});
-
 </script>
 
 <template>
   <UPage>
-    <UPageHero title="Developer Conferences Agenda" description="Explore upcoming developer conferences for 2025."
+    <UPageHero
+      title="Developer Conferences Agenda"
+      description="Explore upcoming developer conferences for 2025."
       :links="[{ label: 'Calendar', to: '#calendar', size: 'xl' }]" />
     
     <UPageSection id="calendar" title="Conferences" :description="`${conferences.length} events`">
-      <div class="mt-4 mx-auto">
-        <UInputMenu
-          v-model="selectedCountries"
-          :items="countries"
-          multiple
-          placeholder="Filter by country"
-        />
-      </div>
-
-      <div class="flex flex-col md:flex-row justify-center md:space-x-20 space-y-10 md:space-y-0 mt-10 overflow-y-auto">
-        <div class="calendar-wrapper">
-          <UCalendar 
-            v-model="selectedDate" 
-            size="xl"
-            :ui="{ 
-              cellTrigger: 'size-12 text-lg',
-              headCell: 'text-lg py-2',
-              heading: 'text-xl font-bold py-2'
-            }"
-          >
-            <template #day="{ day }">
-              <UChip :show="isConferenceDate(day)" size="xs">
-                {{ day.day }}
-              </UChip>
-            </template>
-          </UCalendar>
-        </div>
-        <UTable :data="conferencesForSelectedDate" :columns="columns" :column-visibility="columnVisiblity">
-        </UTable>
-      </div>
+      <ConferenceCalendar :conferences="conferences" />
     </UPageSection>
     
     <UPageSection>
